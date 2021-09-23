e36b36f1d4564861208cd70a5275265082309d92
Mateusz <mateuszzygadlo00@gmail.com>
2021-09-23 22:09:14 +0200

    changes from require to import

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -1,4 +1,4 @@
-const reverseString = require('../scripts/reverseString');
+import reverseString from '../scripts/reverseString';
 
 test('reverse string', () => {
     expect(reverseString('sentence')).toBe('ecnetnes');
